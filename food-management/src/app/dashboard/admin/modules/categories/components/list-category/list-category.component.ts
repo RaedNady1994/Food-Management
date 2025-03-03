ccaaa39d6096253f60a7ba6b0f8efc476980cb7d
RaedNady1994 <engraednady@gmail.com>
2025-03-03 16:25:00 +0200

    adding and updating category

--- food-management/src/app/dashboard/admin/modules/categories/components/list-category/list-category.component.ts
+++ food-management/src/app/dashboard/admin/modules/categories/components/list-category/list-category.component.ts
@@ -1,44 +1,102 @@
-import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from '@angular/core';
 import { ICategoriesResponse, IGetCategoriesRequest } from '../../interfaces/icategory';
 import { CategoryService } from '../../services/category.service';
 import { IPagedResponse } from 'src/app/core/interfaces/ipaged-response';
 import { AddEditCategoryComponent } from '../add-edit-category/add-edit-category.component';
-import {MatDialog} from '@angular/material/dialog'
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-list-category',
   templateUrl: './list-category.component.html',
   styleUrls: ['./list-category.component.scss'],
 })
-export class ListCategoryComponent {
-  
-  private gerCategoriesParams: IGetCategoriesRequest = {
+export class ListCategoryComponent implements OnInit {
+  categoriesParams: IGetCategoriesRequest = {
     page: 1,
     size: 10,
   };
 
-  getCategoriesResponse!: IPagedResponse<ICategoriesResponse>
+  categoriesResponse!: IPagedResponse<ICategoriesResponse>;
 
-  constructor(private service: CategoryService, public dialog: MatDialog) {
-    this.initiateCategoryData();
+  constructor(
+    private categoryService: CategoryService,
+    private dialog: MatDialog,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCategories();
   }
 
-  initiateCategoryData() {
-    this.service.getCategories(this.gerCategoriesParams).subscribe((res) => {
-      this.getCategoriesResponse = res;
+  loadCategories(): void {
+    this.categoryService.getCategories(this.categoriesParams).subscribe({
+      next: (res) => {
+        this.categoriesResponse = res;
+      },
+      error: (err) => {
+        this.toastr.error(err.error?.message || 'Failed to load categories', 'Error');
+      },
     });
   }
 
-  openAddEditCategoryDialog(): void {
+  openAddCategoryDialog(): void {
     const dialogRef = this.dialog.open(AddEditCategoryComponent, {
-      width: '50%', 
-      minWidth:'350px',
-      data: {name:''},
+      width: '50%',
+      minWidth: '350px',
+      data: { name: '' },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        alert('rrr');
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result?.name) {
+        this.addCategory(result.name);
       }
+    });
+  }
+
+  addCategory(name: string): void {
+    this.categoryService.addCategory({ name }).subscribe({
+      next: () => {
+        this.toastr.success('Category created successfully', 'Success');
+        this.loadCategories();
+      },
+      error: (err) => {
+        this.toastr.error(err?.error?.message || 'Error adding category', 'Error');
+      },
+    });
+  }
+
+  openEditCategoryDialog(category: ICategoriesResponse): void {
+    const dialogRef = this.dialog.open(AddEditCategoryComponent, {
+      width: '50%',
+      minWidth: '350px',
+      data: { name: category.name },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result?.name) {
+        this.updateCategory(category.id, { name: result.name });
+      }
+    });
+  }
+
+  updateCategory(id: number, data: { name: string }): void {
+    this.categoryService.updateCategory(id, data).subscribe({
+      next: () => {
+        this.toastr.success('Category updated successfully', 'Success');
+        this.loadCategories();
+      },
+      error: (err) => {
+        this.toastr.error(err?.error?.message || 'Error updating category', 'Error');
+      },
+    });
+  }
+
+  openViewCategoryDialog(category: ICategoriesResponse): void {
+    const dialogRef = this.dialog.open(AddEditCategoryComponent, {
+      width: '50%',
+      minWidth: '350px',
+      data: { name: category.name, isReadOnly: true },
     });
   }
 }
--- food-management/src/app/dashboard/admin/modules/categories/services/category.service.ts
+++ food-management/src/app/dashboard/admin/modules/categories/services/category.service.ts
@@ -2,16 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ICategoriesResponse, IGetCategoriesRequest } from '../interfaces/icategory';
-import { IPagedResponse } from '../../../../../core/interfaces/ipaged-response';
+import { IPagedResponse } from 'src/app/core/interfaces/ipaged-response';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CategoryService {
+export class CategoryService { 
+
   constructor(private http: HttpClient) {}
 
   getCategories(request: IGetCategoriesRequest): Observable<IPagedResponse<ICategoriesResponse>> {
+    return this.http.get<IPagedResponse<ICategoriesResponse>>(
+      `Category/?page=${request.page}&size=${request.size}`
+    );
+  }
 
-    return this.http.get<IPagedResponse<ICategoriesResponse>>( `Category/?page=${request.page}&size=${request.size}`);
+  addCategory(data: { name: string }): Observable<ICategoriesResponse> {
+    return this.http.post<ICategoriesResponse>(`Category/`, data);
+  }
+
+  updateCategory(id: number, data: { name: string }): Observable<ICategoriesResponse> {
+    return this.http.put<ICategoriesResponse>(`Category/${id}`, data);
   }
 }
